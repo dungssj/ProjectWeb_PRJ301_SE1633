bbb53d0b77b150fd7531ab4fa5fefce73698198a
dungssj <dungldqhe161262@fpt.edu.vn>
2022-07-14 12:14:59 +0700

    Add if user not login yet then redirect to login page

--- src/java/controller/AccountSettingServlet.java
+++ src/java/controller/AccountSettingServlet.java
@@ -54,6 +54,9 @@ public class AccountSettingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+        if(request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("Login.jsp");
+        }
         User u =(User) request.getSession().getAttribute("user");
         request.setAttribute("user", u);
         request.getRequestDispatcher("AccountInformation.jsp").forward(request, response);
